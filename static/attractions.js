048ae3c1567d8b22f046872f1871af57e718f778
chienhuak <christy.kch.c@gmail.com>
2024-06-13 14:30:56 +0800

    Part 2-4, Part 2-5 : debug
    Part 2-4 : debug search feature, when scroll down photos disappear ( set container2.innertext = "" to correct place)
    Part 2-5 : add mrt search feature

--- static/attractions.js
+++ static/attractions.js
@@ -1,11 +1,15 @@
+let debounceTimer; // 防彈跳計時器，防止事件過度觸發
+let currentKeyword = ""; // 當前關鍵字
+let nextPage
+
 // 查詢功能，監聽查詢輸入框
 const searchForm = document.querySelector('.searchbar');
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault(); // 防止表單提交
     const searchInput = document.querySelector('#search-input');
     currentKeyword = searchInput.value.trim();
-    currentPage = 0; // 重置頁數
-    photos(currentPage, currentKeyword); // 根據新的關鍵字加載照片
+    container2.innerHTML = ""
+    photos(0, currentKeyword); // 重置頁數 // 根據新的關鍵字加載照片
 });
 
 
@@ -15,8 +19,14 @@ function mrts() {
     .then(response => response.json())
     .then(data => {
         for (let i=0;i<data.data.length;i++){
-            const mrt = document.createElement('a')
+            const mrt = document.createElement('button')
+            mrt.className = "mrt"
             mrt.innerText = data.data[i]
+            mrt.addEventListener('click', (event) => {
+                currentKeyword = mrt.innerText
+                container2.innerHTML = ""
+                photos(0, currentKeyword) 
+            })
             div.appendChild(mrt)
         }
     })
@@ -43,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function photos(page, keyword = "") {
+    console.log(page,keyword);
     //使用模板字面量（template literals）構建URL時，需使用反引號（`）而不是單引號（'）。這樣可以確保變量 ${page} 被正確替換。
     let apiUrl;
     if (keyword) {
@@ -56,34 +67,35 @@ function photos(page, keyword = "") {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // 如果是新的關鍵字搜索（頁數為0），則清空容器
-            if (keyword) {
-                container2.innerHTML = "";
-            }
-
             for (let i=0;i<data.data.length;i++){
                 const div = document.createElement('div')
                 div.className = "imgbox"
+                const textdiv = document.createElement('div')
+                textdiv.className = "textbox"
                 const p1 = document.createElement('p')
                 p1.className = "p1"
                 p1.innerText = data.data[i].name  
-                const p2 = document.createElement('p')
-                p2.className = "p2"
-                p2.innerText = data.data[i].mrt  
+                const s2 = document.createElement('span')
+                s2.className = "s2"
+                s2.innerText = data.data[i].mrt  
+                const s3 = document.createElement('span')
+                s3.className = "s3"
+                s3.innerText = "公共藝術"  
                 const photo = document.createElement('img')
                 photo.className = "myphoto"
                 photo.src = data.data[i].images[0]     
                 photo.innerText          
                 div.appendChild(photo)
-                div.appendChild(p1)
-                div.appendChild(p2)
+                div.appendChild(p1)                
                 container2.appendChild(div)
+                div.appendChild(textdiv)
+                textdiv.appendChild(s2)
+                textdiv.appendChild(s3)
             }
-            loading = false; // 重置loading狀態
+            nextPage = data.nextPage; 
         })
         .catch(error => {
             console.error('Error:', error)
-            loading = false; // 請求失敗，重置loading狀態
         });
 }
 
@@ -95,19 +107,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 // 監聽滾動事件
-let currentPage = 0; // 初始化頁數
-let loading = false; // 請求狀態
-let debounceTimer; // 防彈跳計時器，防止事件過度觸發
-let currentKeyword = ""; // 當前關鍵字
-
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 && !loading) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
         // 滾動到接近底部500px且沒有正在進行的請求時加載更多照片
-        loading = true;
-        currentPage++;
+        if(!nextPage) return;  //最後一頁 nextPage null則跳出程式
         clearTimeout(debounceTimer); // 清除之前的計時器
         debounceTimer = setTimeout(() => {
-            photos(currentPage,keyword);
+            photos(nextPage,currentKeyword);
         }, 500); // 在500毫秒後執行加載操作
     }
 });
