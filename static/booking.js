150d1299f7fb1e0125543a1fd2a8f185fd3899d0
chienhuak <christy.kch.c@gmail.com>
2024-07-13 04:08:47 +0800

    Part 5-x : decode jwt in javascript, and render username to booking page

--- static/booking.js
+++ static/booking.js
@@ -172,3 +172,30 @@ function deleteitem(event) {
     })
 
 }
+
+
+// decode JWT
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+
+function renderName() {
+    const namespan = document.getElementById('booking_user');
+    const token = localStorage.getItem('token');
+    if (token) {
+        try {
+            const decodedToken = parseJwt(token);
+            const name = decodedToken.name;
+            namespan.innerText = name;
+        } catch (error) {
+            console.error('Error decoding JWT:', error);
+        }
+    }
+}
